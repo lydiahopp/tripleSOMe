79445bac5e37678d49d40e0679abb143b35815bc
Lydia Hopp <lydiahopp@freenet.de>
2019-05-16 14:04:58 +0200

    Update variance_spots.r
    Removed an error in calculation of variance spots

--- R/variance_spots.r
+++ R/variance_spots.r
@@ -6,130 +6,148 @@ variance.spots=function()
   environment(modules.chromosomes) <- environment()
   environment(modules.relations) <- environment()
 
-  
-  
-  ##### variance Spots ######
   uh <- matrix(log10(apply(metadata, 1, var)), preferences$dim.1stLvlSom, preferences$dim.1stLvlSom)
 
-  peak.matrix <- matrix( FALSE, preferences$dim.1stLvlSom, preferences$dim.1stLvlSom )
+  metadata.scaled <- (uh-min(uh)) / (max(uh)-min(uh))
 
-  for( pos.x in 1:preferences$dim.1stLvlSom )
-    for( pos.y in 1:preferences$dim.1stLvlSom )
-    {
-      neighbor.dists <- sapply( get.neighbors(pos.x, pos.y, preferences$dim.1stLvlSom), function(x) uh[x[1],x[2]] )
-      peak.matrix[pos.x, pos.y] <- all( uh[ pos.x, pos.y] <= neighbor.dists )
-    }
-  
-  spot.matrix <- matrix(0, preferences$dim.1stLvlSom, preferences$dim.1stLvlSom )
-  for( sel.minimum in which(peak.matrix)[order( uh[ which(peak.matrix) ], decreasing=TRUE )] )
+  ##### variance Spots ######
+
+  # extract sample modules
+  sample.spot.list <- list()
+  sample.spot.core.list <- list()
+  n.sample.modules <- 0
+  preferences$spot.threshold.modules=0.88
+
+  #for (m in 1:ncol(indata))
+  #{
+  # define bigger core regions
+  core <- matrix(NA, preferences$dim.1stLvlSom, preferences$dim.1stLvlSom)
+  core[which(metadata.scaled> preferences$spot.threshold.modules)] <- -1
+
+  spot.i = 0
+
+  while (nrow(which(core == -1, arr.ind=TRUE)) > 0)
   {
-    spot.members <- c( sel.minimum )
-    spot.d <- c( mean( uh[spot.members] ) ) 
-    
-    while(TRUE)
-    {
-      spot.neighbors <- unique( unlist(   sapply( spot.members, get.neighbors, dim=preferences$dim.1stLvlSom )  ) )
-      spot.neighbors <- setdiff( spot.neighbors, spot.members )
-      
-      expansion.d <- sapply( spot.neighbors, function(x) dist( metadata[c(x,sel.minimum),] ) )
-      expansion <- spot.neighbors[ which.min( expansion.d ) ]
-      
-      if( ( mean( uh[c(spot.members,expansion)] ) < spot.d[length(spot.d)] && 
-            spot.d[length(spot.d)] < spot.d[length(spot.d)-1] && 
-            spot.d[length(spot.d)-1] < spot.d[length(spot.d)-2] ) || 
-          length(spot.members) > preferences$dim.1stLvlSom^2*0.1 ) 
-      {
-        break
-      } else
-      {
-        spot.members <- c( spot.members, expansion )
-        spot.d <- c( spot.d, mean( uh[spot.members] ) ) 
-      }
-      
-    }
-#    if( mean(uh[spot.members]) > mean(uh) ) 
-    spot.matrix[spot.members] <- max(spot.matrix,na.rm=TRUE)+1
+    start.pix <- which(core == -1, arr.ind=TRUE)[1,]
+    spot.i <- spot.i + 1
+    core <- col.pix(core, start.pix[1], start.pix[2], spot.i, preferences$dim.1stLvlSom)
   }
-  spot.matrix[which(spot.matrix==0)] <- NA
-  
-  
-  
+
+  # shrink each separate region to core size
+  for (s.i in 1:max(core, na.rm=TRUE))
+  {
+    if (sum(core == s.i, na.rm=TRUE) > preferences$spot.coresize.modules)
+    {
+      core.metagenes <- which(core == s.i)
+      o <- order(uh[core.metagenes], decreasing=TRUE)[1:preferences$spot.coresize.modules]
+      core[setdiff(core.metagenes, core.metagenes[o])] <- NA
+    }
+  }
+
+  core[which(!is.na(core))] <- -1
+  spot.i <- 0
+
+  while (nrow(which(core == -1, arr.ind=TRUE)) > 0)
+  {
+    start.pix <- which(core == -1, arr.ind=TRUE)[1,]
+    spot.i <- spot.i + 1
+    core <- col.pix(core, start.pix[1], start.pix[2], spot.i, preferences$dim.1stLvlSom)
+  }
+
+  # define spot area around cores
+  for (s.i in 1:max(core,na.rm=TRUE))
+  {
+    n.sample.modules <- n.sample.modules + 1
+
+    sample.spot.core.list[[n.sample.modules]] <-
+      matrix(NA, preferences$dim.1stLvlSom, preferences$dim.1stLvlSom)
+
+    sample.spot.core.list[[n.sample.modules]][which(core == s.i)] <-
+      uh[which(core == s.i)]
+
+    spot <- matrix(NA, preferences$dim.1stLvlSom, preferences$dim.1stLvlSom)
+    spot[which(metadata.scaled > preferences$spot.threshold.modules)] <- -1
+
+    start.pix <- which(!is.na(sample.spot.core.list[[n.sample.modules]]), arr.ind=TRUE)
+    start.pix <- start.pix[which.max(sample.spot.core.list[[n.sample.modules]][start.pix]),]
+
+    spot <- col.pix(spot, start.pix[1], start.pix[2], 1, preferences$dim.1stLvlSom)
+
+    sample.spot.list[[n.sample.modules]] <- matrix(NA, preferences$dim.1stLvlSom, preferences$dim.1stLvlSom)
+    sample.spot.list[[n.sample.modules]][which(spot == 1)] <- metadata.scaled[which(spot == 1)]
+  }
+
+
+  # filter
+  remove <- c()
+
+  for (i in 1:n.sample.modules)
+  {
+    if (sum(!is.na(sample.spot.list[[i]])) <= 1)
+    {
+      # empty, i.e. does not exceed threshold -> remove
+      remove <- c(remove, i)
+    } else if (sum(!is.na(sample.spot.list[[i]])) < sum(!is.na(sample.spot.core.list[[i]])))
+    {
+      # core larger than spot -> shrink core
+      sample.spot.core.list[[i]] <- sample.spot.list[[i]]
+    }
+  }
+
+  if (length(remove) > 0)
+  {
+    sample.spot.list <- sample.spot.list[-remove]
+    sample.spot.core.list <- sample.spot.core.list[-remove]
+    n.sample.modules <- length(sample.spot.list)
+  }
+
+  o <- order(sapply(sample.spot.core.list, function(x) { mean(x, na.rm=TRUE) }), decreasing=TRUE)
+  sample.spot.list <- sample.spot.list[o]
+  sample.spot.core.list <- sample.spot.core.list[o]
+
+
+  ## define variance spots ##
   spot.list.variance <<- list()
-  spot.list.variance$overview.map <<- uh
+
+  spot.list.variance$overview.map <<-uh
+
   spot.list.variance$overview.mask <<- rep(NA, preferences$dim.1stLvlSom ^ 2)
   spot.list.variance$spots <<- list()
-  
-  count.cluster <- 1
-  for (i in seq_along(sort(unique(na.omit(as.vector(spot.matrix))))) )
+
+  for (i in seq_along(sample.spot.list))
   {
-    spot.metagenes <- which(spot.matrix==i)
+    spot.metagenes <- which(!is.na(sample.spot.list[[i]]))
     spot.genes <- rownames(indata)[which(som.result$feature.BMU %in% spot.metagenes)]
-    
+
     if (length(spot.genes) > 0)
     {
-      spot.list.variance$overview.mask[spot.metagenes] <<- count.cluster
-      spot.list.variance$spots[[LETTERS[count.cluster]]] <<- list()
-      spot.list.variance$spots[[LETTERS[count.cluster]]]$metagenes <<- spot.metagenes
-      spot.list.variance$spots[[LETTERS[count.cluster]]]$genes <<- spot.genes
-      spot.list.variance$spots[[LETTERS[count.cluster]]]$mask <<- rep(NA, preferences$dim.1stLvlSom * preferences$dim.1stLvlSom)
-      spot.list.variance$spots[[LETTERS[count.cluster]]]$mask[spot.metagenes] <<- 1
-      
-      spot.list.variance$spots[[LETTERS[count.cluster]]]$position <<-
-        colMeans(apply(som.result$node.summary[spot.metagenes, 1:2]+1, 2, range))
-      
-      spot.list.variance$spots[[LETTERS[count.cluster]]]$beta.statistic <<-
-        get.beta.statistic(set.data=metadata[spot.list.variance$spots[[LETTERS[count.cluster]]]$metagenes,,drop=FALSE],
-                           weights=som.result$node.summary[spot.list.variance$spots[[LETTERS[count.cluster]]]$metagenes,]$n.features)
-      
-      count.cluster <- count.cluster + 1
+      spot.list.variance$overview.mask[which(!is.na(sample.spot.list[[i]]))] <<- i
+      spot.list.variance$spots[[LETTERS[i]]] <<- list()
+      spot.list.variance$spots[[LETTERS[i]]]$metagenes <<- spot.metagenes
+      spot.list.variance$spots[[LETTERS[i]]]$genes <<- spot.genes
+      spot.list.variance$spots[[LETTERS[i]]]$mask <<- rep(NA, preferences$dim.1stLvlSom * preferences$dim.1stLvlSom)
+      spot.list.variance$spots[[LETTERS[i]]]$mask[spot.metagenes] <<- 1
+
+      spot.list.variance$spots[[LETTERS[i]]]$position <<-
+        colMeans(apply(som.result$node.summary[spot.metagenes, 1:2] + 1, 2, range))
+
+      spot.list.variance$spots[[LETTERS[i]]]$beta.statistic <<-
+        get.beta.statistic(set.data=metadata[spot.list.variance$spots[[LETTERS[i]]]$metagenes,,drop=FALSE],
+                           weights=som.result$node.summary[spot.list.variance$spots[[LETTERS[i]]]$metagenes,]$n.features)
     }
   }
-  
-  spot.list.variance$spotdata <<-
-    t(sapply(spot.list.variance$spots, function(x)
-    {
-      if (length(x$genes > 0))
-      {
-        colMeans(indata[x$genes,,drop=FALSE],na.rm=TRUE)
-      } else
-      {
-        rep(0, ncol(indata))
-      }
-    }))
-  
-  sig.spots <- which( apply( spot.list.variance$spotdata, 1, function(x) sd(x) > sd(spot.list.variance$spotdata,na.rm=T) ) )
-  if( length(sig.spots) > 0 )
+
+  o <- order(sapply(spot.list.variance$spots, function(x)
   {
-    spot.list.variance$spots <<- spot.list.variance$spots[sig.spots]
-    spot.list.variance$overview.mask[which(!spot.list.variance$overview.mask%in%sig.spots)] <<- NA
-    spot.list.variance$overview.mask[!is.na(spot.list.variance$overview.mask  )] <<-
-      match(spot.list.variance$overview.mask[!is.na(spot.list.variance$overview.mask)], sort(unique(na.omit(as.vector(spot.list.variance$overview.mask)))))
-  }
-  
-  start.spot <- which.min( apply( sapply(spot.list.variance$spots, function(x) x$position ), 2, min ) )
-  
-  spot.arcs <- sapply(spot.list.variance$spots, function(x)
-  {
-    -atan2(x$position['y'] - preferences$dim.1stLvlSom / 2, x$position['x'] - preferences$dim.1stLvlSom / 2)
-  })
-  
-  spot.arcs <- spot.arcs - spot.arcs[start.spot]
-  
-  if (any(spot.arcs<0))
-  {
-    spot.arcs[which(spot.arcs<0)] <- spot.arcs[which(spot.arcs<0)] + (2 * pi)
-  }
-  
-  
-  
-  o <- order(spot.arcs)
-  
+    which.max(apply(metadata[x$metagenes,,drop=FALSE], 2, mean))
+  }))
+
   spot.list.variance$spots <<- spot.list.variance$spots[o]
   names(spot.list.variance$spots) <<- LETTERS[seq_along(spot.list.variance$spots)]
-  
-  spot.list.variance$overview.mask[!is.na(spot.list.variance$overview.mask  )] <<-
-    match(spot.list.variance$overview.mask[!is.na(spot.list.variance$overview.mask)], sort(unique(na.omit(as.vector(spot.list.variance$overview.mask))))[o])
-  
+
+  spot.list.variance$overview.mask[!is.na(spot.list.variance$overview.mask)] <<-
+    match(spot.list.variance$overview.mask[!is.na(spot.list.variance$overview.mask)], o)
+
   spot.list.variance$spotdata <<-
     t(sapply(spot.list.variance$spots, function(x)
     {
@@ -141,10 +159,9 @@ variance.spots=function()
         rep(0, ncol(indata))
       }
     }))
-  
+
   colnames(spot.list.variance$spotdata) <<- colnames(indata)
-  
-  
+
 
   spot.fisher.p <- function(spot)
   {
@@ -155,20 +172,6 @@ variance.spots=function()
   }
 
   spot.list.variance$spots =lapply( spot.list.variance$spots, spot.fisher.p)
-
-  #spot.list.group.underexpression2=spot.list.group.underexpression
-
-  #rm(list=ls()[-which(ls()=="spot.list.group.underexpression2")])
-  #detach(env)
-  # load("ScoV.RData")
-  #env$spot.list.group.underexpression=NULL
-  #env$spot.list.group.underexpression=spot.list.group.underexpression2
-  # save(env, file="ScoV.RData")
-
-  # rm(list=ls())
-  # load("ScoV.RData")
-  #attach(env)
-  #library(oposSOM)
 
   dirname <- file.path( paste(files.name, "- Results"),"Summary Sheets - Modules","Variance Spots" )
   util.info("Writing:", file.path(dirname, "*.pdf"))
